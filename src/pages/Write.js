a4182d188206183047be481aa6e281fa4ab401e5
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-07-19 16:30:19 +0900

    ✨write 마크다운 언어 기능
    작성페이지 마크다운 언어기능입니다.

--- src/pages/Write.js
+++ src/pages/Write.js
@@ -1,12 +1,25 @@
 import React from "react";
+import '@toast-ui/editor/dist/toastui-editor.css';
+import { Editor } from '@toast-ui/react-editor';
 
 const Write = (props) => {
-
-    return (
-        <React.Fragment>
-            
-        </React.Fragment>
-    );
-}
+  const editor = new Editor({
+    el: document.querySelector("#editor"),
+    height: "600px",
+    initialEditType: "markdown",
+    previewStyle: "vertical",
+  });
+  return (
+    <React.Fragment>
+      <Editor
+        initialValue="hello react editor world!"
+        previewStyle="vertical"
+        height="600px"
+        initialEditType="markdown"
+        useCommandShortcut={true}
+      />
+    </React.Fragment>
+  );
+};
 
 export default Write;
